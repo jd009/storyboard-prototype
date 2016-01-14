130a81c7af9a365c0d5010f1a37178c3f8cdada4
John D <deblanc.john@gmail.com>
2016-01-14 17:08:38 -0600

    Making a more generic playNextVideo and recueCurrentVideo functions.  This allows me to generalize the player event listener.

--- client/js/app.js
+++ client/js/app.js
@@ -9,13 +9,16 @@ function onYouTubeIframeAPIReady() {
   player1 = new YT.Player('player1', {
     height: VIDEO_HEIGHT,
     width: VIDEO_WIDTH,
+    videoId: '8lXdyD2Yzls', //gopher
     playerVars: {
       controls: 0,
-      showinfo: 0
+      showinfo: 0,
+      start: 0,
+      end: 2
     },
     events: {
       'onReady': storyStateMachine.onPlayer1Ready.bind(storyStateMachine),
-      'onStateChange': storyStateMachine.frame1StateListener.bind(storyStateMachine)
+      'onStateChange': storyStateMachine.playerStateListener.bind(storyStateMachine)
     }
     }
   );
@@ -23,13 +26,16 @@ function onYouTubeIframeAPIReady() {
   player2 = new YT.Player('player2', {
     height: VIDEO_HEIGHT,
     width: VIDEO_WIDTH,
+    videoId: '3GJOVPjhXMY', //star wars kid
     playerVars: {
       controls: 0,
-      showinfo: 0
+      showinfo: 0,
+      start: 9,
+      end: 14
     },
     events: {
       'onReady': storyStateMachine.onPlayer2Ready.bind(storyStateMachine),
-      'onStateChange': storyStateMachine.frame2StateListener.bind(storyStateMachine)
+      'onStateChange': storyStateMachine.playerStateListener.bind(storyStateMachine)
     }
     }
   );
@@ -37,20 +43,23 @@ function onYouTubeIframeAPIReady() {
   player3 = new YT.Player('player3', {
     height: VIDEO_HEIGHT,
     width: VIDEO_WIDTH,
+    videoId: '-5x5OXfe9KY', //baby
     playerVars: {
       controls: 0,
-      showinfo: 0
+      showinfo: 0,
+      start: 3,
+      end: 7
     },
     events: {
       'onReady': storyStateMachine.onPlayer3Ready.bind(storyStateMachine),
-      'onStateChange': storyStateMachine.frame3StateListener.bind(storyStateMachine)
+      'onStateChange': storyStateMachine.playerStateListener.bind(storyStateMachine)
     }
     }
   );
 }
 
 function StoryStateMachine(){
-  this.frame1StateListener = function(event){
+  this.playerStateListener = function(event){
     var state;
     switch(event.data){
       case -1:
@@ -64,112 +73,61 @@ function StoryStateMachine(){
         break;
       case YT.PlayerState.PAUSED:
         state = 'paused';
+        this.playNextVideo(event);
+        this.recueCurrentVideo(event);
+        break;
+      case YT.PlayerState.BUFFERING:
+        state = 'buffering';
+        break;
+      case YT.PlayerState.CUED:
+        state = 'cued';
+        break;
+    }
+    var playerDivId = event.target.f.id;
+    console.log(playerDivId);
+    console.log('Current url: ' + event.target.B.videoUrl);
+    console.log('Current State: ' + state);
+  };
+
+  this.playNextVideo = function(event){
+    var pausedPlayer = event.target.f.id;
+    switch(pausedPlayer){
+      case 'player1':
         player2.playVideo();
-        event.target.cueVideoById(
-          {
-            'videoId': '8lXdyD2Yzls', //gopher
-            'startSeconds': 0,
-            'endSeconds': 4
-          }
-        );
         break;
-      case YT.PlayerState.BUFFERING:
-        state = 'buffering';
-        break;
-      case YT.PlayerState.CUED:
-        state = 'cued';
-        break;
-    }
-    console.log('Frame 1');
-    console.log('Current url: ' + event.target.B.videoUrl);
-    console.log('Current State: ' + state);
-  };
-
-  this.frame2StateListener = function(event){
-    var state;
-    switch(event.data){
-      case -1:
-        state = 'unstarted';
-        break;
-      case YT.PlayerState.ENDED:
-        state = 'ended';
-        break;
-      case YT.PlayerState.PLAYING:
-        state = 'playing';
-        break;
-      case YT.PlayerState.PAUSED:
-        state = 'paused';
+      case 'player2':
         player3.playVideo();
-        this.onPlayer2Ready(event);
         break;
-      case YT.PlayerState.BUFFERING:
-        state = 'buffering';
-        break;
-      case YT.PlayerState.CUED:
-        state = 'cued';
+      case 'player3':
         break;
     }
-    console.log('Frame 2');
-    console.log('Current url: ' + event.target.B.videoUrl);
-    console.log('Current State: ' + state);
-  };
+  }
 
-  this.frame3StateListener = function(event){
-    var state;
-    switch(event.data){
-      case -1:
-        state = 'unstarted';
-        break;
-      case YT.PlayerState.ENDED:
-        state = 'ended';
-        break;
-      case YT.PlayerState.PLAYING:
-        state = 'playing';
-        break;
-      case YT.PlayerState.PAUSED:
-        state = 'paused';
-        this.onPlayer3Ready(event);
-        break;
-      case YT.PlayerState.BUFFERING:
-        state = 'buffering';
-        break;
-      case YT.PlayerState.CUED:
-        state = 'cued';
-        break;
-    }
-    console.log('Frame 3');
-    console.log('Current url: ' + event.target.B.videoUrl);
-    console.log('Current State: ' + state);
-  };
-
-  this.onPlayer1Ready = function(event){
-    event.target.loadVideoById(
+  this.recueCurrentVideo = function(event){
+    var currentVideoInfo = event.target.h.h;
+    var currentVideoId = currentVideoInfo.videoId;
+    var currentVideoStart = currentVideoInfo.playerVars.start;
+    var currentVideoEnd = currentVideoInfo.playerVars.end;
+    event.target.cueVideoById(
       {
-        'videoId': '8lXdyD2Yzls', //gopher
-        'startSeconds': 0,
-        'endSeconds': 4
+        'videoId': currentVideoId,
+        'startSeconds': currentVideoStart,
+        'endSeconds': currentVideoEnd
       }
     );
+  }
+
+  this.onPlayer1Ready = function(event){
+    event.target.playVideo();
   }
 
   this.onPlayer2Ready = function(event){
-    event.target.cueVideoById(
-      {
-        'videoId': '3GJOVPjhXMY', //star wars kid
-        'startSeconds': 9,
-        'endSeconds': 14
-      }
-    );
+    recueCurrentVideo(event);
+
   }
 
   this.onPlayer3Ready = function(event){
-    event.target.cueVideoById(
-      {
-        'videoId': '-5x5OXfe9KY', //dancing baby
-        'startSeconds': 3,
-        'endSeconds': 7
-      }
-    );
+    recueCurrentVideo(event);
   }
 }
 
